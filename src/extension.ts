b9e8f8e3e0e8c3ffe88d27f059189f7601d51c5d
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-07-15 10:32:49 +0530

    Adding a webview

--- src/extension.ts
+++ src/extension.ts
@@ -3,22 +3,33 @@ import parser from "./parser";
 import visualize from "./vizualizer";
 import { dependencyGraph } from "./types";
 export async function activate(context: vscode.ExtensionContext) {
-  if (vscode.workspace.workspaceFolders) {
-    const folders = vscode.workspace.workspaceFolders;
-    let mainFolder: string = "";
-    mainFolder = folders[0].uri.path;
-    mainFolder = mainFolder.replace(mainFolder[0], "");
-    const viewGraphs: Array<dependencyGraph> | undefined = await parser(
-      mainFolder
-    );
-    if (!viewGraphs) {
-      vscode.window.showErrorMessage("SRC directory not found.");
-      return;
-    }
-    await visualize(viewGraphs);
-  } else {
-    vscode.window.showErrorMessage("Please open a workspace");
-  }
+  context.subscriptions.push(
+    vscode.commands.registerCommand("visualize.start", async () => {
+      // Create and show a new webview
+      const panel = vscode.window.createWebviewPanel(
+        "visualize",
+        "Generator Graph",
+        vscode.ViewColumn.One,
+        { enableScripts: true }
+      );
+      if (vscode.workspace.workspaceFolders) {
+        const folders = vscode.workspace.workspaceFolders;
+        let mainFolder: string = "";
+        mainFolder = folders[0].uri.path;
+        mainFolder = mainFolder.replace(mainFolder[0], "");
+        const viewGraphs: Array<dependencyGraph> | undefined = await parser(
+          mainFolder
+        );
+        if (!viewGraphs) {
+          vscode.window.showErrorMessage("SRC directory not found.");
+          return;
+        }
+        panel.webview.html = await visualize(viewGraphs);
+      } else {
+        vscode.window.showErrorMessage("Please open a workspace");
+      }
+    })
+  );
 }
 
 export function deactivate() {}
--- src/vizualizer.ts
+++ src/vizualizer.ts
@@ -1,9 +1,20 @@
 import { dependencyGraph } from "./types";
 
 export default async function visualize(
-  viewGraphs: Array<dependencyGraph> | undefined
-): Promise<void> {
-  console.log({ viewGraphs });
-
-  return;
+  viewGraphs: Array<dependencyGraph>
+): Promise<string> {
+  for (const viewGraph of viewGraphs) {
+    console.log(viewGraph);
+  }
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
 }
