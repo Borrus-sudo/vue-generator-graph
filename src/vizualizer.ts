93a0dfa3e0fe2849a9c54f98300430924c46c006
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-07-16 12:57:08 +0530

    Bug fixing

--- src/vizualizer.ts
+++ src/vizualizer.ts
@@ -2,40 +2,39 @@ import { dependencyGraph } from "./types";
 /*@ts-ignore */
 // import * as html from "./web/index.html";
 
-const createGraphs = async (
+const createGraphs = (
   viewGraphs: Array<{
     name: string;
     graph: dependencyGraph;
   }>
-): Promise<string> => {
+): string => {
   const cache = new Map();
   const createNodeGraph = (node: {
     name: string;
     graph: dependencyGraph | "none" | "circularReference";
   }): string => {
-    let currentScript = `${node.name} \n`;
+    let currentScript = ``;
     if (node.graph != "circularReference" && node.graph != "none") {
       for (let module of node.graph.moduleImports) {
         currentScript += createNodeGraph(module);
-        console.log(`${node.name}-->${module.name} \n`);
-        currentScript += `${node.name}-->${module.name} \n`;
+        currentScript += `\t ${node.name}-->${module.name} \n`;
       }
     }
     return currentScript;
   };
   for (const viewGraph of viewGraphs) {
-    const mermaidMD = `graph ` + createNodeGraph(viewGraph);
+    const mermaidMD = `graph TD \n` + createNodeGraph(viewGraph);
     console.log(mermaidMD);
   }
   return "";
 };
-export default async function visualize(
+export default function visualize(
   viewGraphs: Array<{
     name: string;
     graph: dependencyGraph;
   }>
-): Promise<string> {
-  await createGraphs(viewGraphs);
+): string {
+  createGraphs(viewGraphs);
   // console.log(html);
   return `<h1>Hello World</h1>`;
 }
